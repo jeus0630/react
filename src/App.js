ff18648f3903cf0514084d6f9d0e3e7277d6a212
jewoo <jeus0630@designfever.com>
2021-11-29 18:20:07 +0900

    docs : app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import {useState} from "react";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const posts = "강남 고기 맛집";
+
+    let [modal, modalChange] = useState(false);
+    let [title,titleFunc] = useState(["남자 코트 추천","남자 코트 추천1","남자 코트 추천2"]);
+    let [like,likeFunc] = useState(0);
+
+    function changeName(){
+        const arr = [...title];
+        arr.sort();
+        titleFunc(arr);
+    }
+
+    function openModal(){
+        if(modal) {
+            modalChange(false);
+            return;
+        }
+
+        modalChange(true);
+    }
+
+    return (
+        <div className="App">
+            <div className="black-nav">
+                <div>개발 Blog</div>
+            </div>
+            <button onClick={changeName}>버튼</button>
+            <div className="list">
+                <h3>{ title[0] } <span onClick={()=>{likeFunc(like+=1)}}>👍</span> {like} </h3>
+                <p>2월 17일 발행</p>
+                <hr/>
+            </div>
+            <div className="list">
+                <h3>{ title[1] }</h3>
+                <p>2월 17일 발행</p>
+                <hr/>
+            </div>
+            <div className="list">
+                <h3>{ title[2] }</h3>
+                <p>2월 17일 발행</p>
+                <hr/>
+            </div>
+
+            <button onClick={openModal}>버튼</button>
+
+            {
+                modal?
+                <Modal></Modal>
+                : null
+            }
+        </div>
+    );
+}
+
+function Modal(){
+    return (
+        <div className="modal">
+            <h2>제목</h2>
+            <p>날짜</p>
+            <p>상세내용</p>
+        </div>
+    )
 }
 
 export default App;
