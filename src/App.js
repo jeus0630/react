5ae71fd681ef6c2799310afcf8fd4f2ce860754a
jewoo <jeus0630@designfever.com>
2021-11-30 12:51:30 +0900

    docs : app.js 리스트 추가 기능

--- src/App.js
+++ src/App.js
@@ -5,22 +5,30 @@ function App() {
     const posts = "강남 고기 맛집";
 
     let [modal, modalChange] = useState(false);
-    let [title,titleFunc] = useState(["남자 코트 추천","남자 코트 추천1","남자 코트 추천2"]);
-    let [like,likeFunc] = useState(0);
+    let [title, titleFunc] = useState(["남자 코트 추천", "남자 코트 추천1", "남자 코트 추천2"]);
+    let [like, likeFunc] = useState(0);
+    let [clickedTitle, clickedTitleFunc] = useState(0);
+    let [inputValue,inputValueFunc] = useState('');
 
-    function changeName(){
+
+    function changeName() {
         const arr = [...title];
         arr.sort();
         titleFunc(arr);
     }
 
-    function openModal(){
-        if(modal) {
-            modalChange(false);
-            return;
-        }
+    function openModal() {
+        modalChange(!modal);
+    }
 
-        modalChange(true);
+    function btnClick(idx){
+        clickedTitleFunc(idx);
+    }
+
+    function save(){
+        const arr = [...title];
+        arr.unshift(inputValue);
+        titleFunc(arr);
     }
 
     return (
@@ -29,37 +37,40 @@ function App() {
                 <div>개발 Blog</div>
             </div>
             <button onClick={changeName}>버튼</button>
-            <div className="list">
-                <h3>{ title[0] } <span onClick={()=>{likeFunc(like+=1)}}>👍</span> {like} </h3>
-                <p>2월 17일 발행</p>
-                <hr/>
-            </div>
-            <div className="list">
-                <h3>{ title[1] }</h3>
-                <p>2월 17일 발행</p>
-                <hr/>
-            </div>
-            <div className="list">
-                <h3>{ title[2] }</h3>
-                <p>2월 17일 발행</p>
-                <hr/>
+            {
+                title.map((el,idx) => {
+                    return (<div className="list" key={idx}>
+                        <h3 onClick={ ()=>{btnClick(idx); openModal();}}>{el} </h3>
+                        <p>2월 17일 발행</p>
+                        <hr/>
+                    </div>)
+                })
+            }
+
+            <div>
+                HTML 잔뜩 있는 곳
+                <div className="publish">
+                    <input onChange={(e)=>{inputValueFunc(e.target.value)}}/>
+                    <button onClick={save}>저장</button>
+                </div>
             </div>
 
             <button onClick={openModal}>버튼</button>
 
             {
-                modal?
-                <Modal></Modal>
-                : null
+                modal ?
+                    <Modal title={title} clickedTitle={clickedTitle}></Modal>
+                    : null
             }
         </div>
     );
 }
 
-function Modal(){
+function Modal(props) {
+    console.log(props);
     return (
         <div className="modal">
-            <h2>제목</h2>
+            <h2>{props.title[props.clickedTitle]}</h2>
             <p>날짜</p>
             <p>상세내용</p>
         </div>
